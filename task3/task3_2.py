6c97f991fc36f78af46dd1e78a5b287428578bd6
RKMSU <ikarus5991@gmail.com>
2016-11-27 16:32:16 +0300

    MOMO2016MSU

--- task3/task3_2.py
+++ task3/task3_2.py
@@ -2,45 +2,6 @@ import numpy as np
 import scipy as sp
 from numpy import linalg as la
 
-def cg(matvec, b, x0, tol=1e-4, max_iter=None, disp=False, trace=False):
-    if max_iter  == None :
-        max_iter = x0.size
-
-    x_sol = x0
-    discrepency = matvec(x_sol) - b
-    direction = -discrepency
-    norm_dis = la.norm(discrepency)
-    n_iter = 0
-
-    norm_r = list()
-
-    while n_iter < max_iter and norm_dis > tol :
-
-        A_d = matvec(direction)
-
-        alpha = norm_dis ** 2 / np.dot(direction, A_d)
-        x_sol = x_sol + alpha * direction
-        discrepency = discrepency + alpha * A_d
-
-        beta = np.dot(discrepency, discrepency) / (norm_dis * norm_dis)
-        direction = -discrepency + beta * direction
-
-        norm_dis = la.norm(discrepency)
-        n_iter = n_iter + 1
-
-        norm_r.append(norm_dis)
-
-        if disp:
-            print('%s %3d %s %5f' % ('#:', n_iter,'   norm_r:',norm_dis))
-
-    status = 0 if norm_dis < tol else 1
-
-    if trace :
-        hist = {'norm_r' : np.array(norm_r)}
-        return x_sol, status, hist
-    else :
-        return x_sol,status
-
 def barrier(X, y, reg_coef, w0_plus, w0_minus, tol=1e-5, tol_inner=1e-7, max_iter=100, max_iter_inner=20, t0=1, gamma=10, c1=1e-4, disp=False, trace=False) :
 
     n = y.size
@@ -49,11 +10,11 @@ def barrier(X, y, reg_coef, w0_plus, w0_minus, tol=1e-5, tol_inner=1e-7, max_ite
     y_ = X.T.dot(y) / n
     tau = t0
 
-    def matvec(p,w_pl = 0, w_mi = 0, t = 1) :
-
-        res = A.dot(p)
-        res = np.array([(1 + (w_mi[i] ** 2) / (w_pl[i] ** 2)) * res[i] for i in range(res.size)])
-        res = t * res + np.array([p / w ** 2 for p, w in zip(p,w_pl)])
+    def matr(A,w_plus,w_minus) :
+        d = w_plus.size
+        res = np.array([A[:,i] * (1 + (w_minus[i] / w_plus[i]) ** 2) for i in range(d)]).T
+        for i in range(d) :
+            res[i][i] += (1 / w_plus[i] ** 2)
         return res
 
     def generate_b(w_plus, w_minus, tau):
@@ -94,17 +55,16 @@ def barrier(X, y, reg_coef, w0_plus, w0_minus, tol=1e-5, tol_inner=1e-7, max_ite
         while norm_g > tol_inner and n_iter_inner < max_iter_inner :
 
             b = generate_b(w_plus, w_minus, tau)
-            p_plus, st = cg(lambda p : matvec(p, w_pl = w_plus, w_mi = w_minus, t = tau), b, np.zeros(d), tol = 1e-8)
+            p_plus = la.solve(matr(tau * A,w_plus,w_minus), b)
             p_minus = np.array([w_m ** 2 * (-2 * reg_coef * tau + 1 / w_p) + w_m - (w_m ** 2) / (w_p ** 2) * p for w_m,w_p,p in zip(w_minus, w_plus, p_plus)])
+            dot = grad_plus.dot(p_plus) + grad_minus.dot(p_minus)
 
             alpha = select_alpha(list(w_plus) + list(w_minus), list(p_plus) + list(p_minus))
             alpha = min(1, 0.95 * alpha)
 
             f = func(w_plus, w_minus, tau)
-            dot = grad_plus.dot(p_plus) + grad_minus.dot(p_minus)
 
             while not armiho(w_plus, w_minus, p_plus, p_minus, f, dot, alpha, tau) :
-                print(n_iter_inner)
                 alpha /= 2
 
             w_plus = w_plus + alpha * p_plus
@@ -117,11 +77,9 @@ def barrier(X, y, reg_coef, w0_plus, w0_minus, tol=1e-5, tol_inner=1e-7, max_ite
             norm_g = la.norm(np.array(list(grad_plus) + list(grad_minus)))
 
             n_iter_inner += 1
-        print(norm_g)
 
         mu = min(1, n * reg_coef / (la.norm(X.dot(w_plus - w_minus) - y, np.inf))) * (X.dot(w_plus - w_minus) - y) / n
         dual_gap = 0.5 / n * la.norm(X.dot(w_plus - w_minus) - y) ** 2 + reg_coef * la.norm(w_plus - w_minus, 1) + n / 2 * la.norm(mu) ** 2 + mu.dot(y)
-        print(dual_gap)
 
         n_iter += 1
 
@@ -130,17 +88,114 @@ def barrier(X, y, reg_coef, w0_plus, w0_minus, tol=1e-5, tol_inner=1e-7, max_ite
             break
 
         tau *= gamma
-    print(tau)
     return w_plus - w_minus, status
+
+
+def subgrad(X, y, reg_coef, w0, tol=1e-2, max_iter=1000, alpha=1,disp=False, trace=False) :
+
+    def subg(w) :
+        return np.array([np.random.uniform(-1,1) if w[i] == 0 else np.sign(w[i]) for i in range(w.size)])
+
+    def func(w) :
+        return 0.5 / n * (la.norm(X.dot(w) - y) ** 2) + reg_coef * la.norm(w, 1)
+
+    n = y.size
+    A = 1 / n * X.T.dot(X)
+    y_ = 1 / n * X.T.dot(y)
+
+    w = w0
+    f = func(w)
+    direction_ = A.dot(w) - y_
+
+    mu = min(1 , reg_coef / la.norm(A.dot(w) - y_), np.inf) * (X.dot(w) - y) / n
+    dual_gap = 0.5 / n * la.norm(X.dot(w) - y) ** 2 + reg_coef * la.norm(w, 1) + 0.5 * n * la.norm(mu) ** 2 + mu.dot(y)
+
+    n_iter = 0
+
+    while dual_gap > tol and n_iter < max_iter :
+
+        w_pred = w
+        f_pred = f
+
+        direction = reg_coef * subg(w) + direction_
+        direction /= la.norm(direction)
+        w = w - alpha / (n_iter + 1) ** 0.5 * direction
+        f = func(w)
+
+        if f_pred > f :
+            mu = min(1 , reg_coef / la.norm(A.dot(w) - y_), np.inf) * (X.dot(w) - y) / n
+            dual_gap = 0.5 / n * la.norm(X.dot(w) - y) ** 2 + reg_coef * la.norm(w, 1) + 0.5 * n * la.norm(mu) ** 2 + mu.dot(y)
+            direction_ = A.dot(w) - y_
+        else :
+            w = w_pred
+            f = f_pred
+
+        n_iter += 1
+
+    status = 0 if dual_gap < tol else 1
+    return w, status
+
+
+
+def prox_grad(X, y, reg_coef, w0, tol=1e-5, max_iter=1000, L0=1, disp=False, trace=False) :
+
+    def prox(x, alpha) :
+        ans = [x[i] + alpha if x[i] < -alpha else x[i] - alpha if x[i] > alpha else 0 for i in range(x.size)]
+        return np.array(ans)
+
+    def func(w) :
+        return 0.5 / n * la.norm(X.dot(w) - y) ** 2 + reg_coef * la.norm(w,1)
+
+    def m_L(y , x, grad, L) :
+         return func(x) + grad.dot(y - x) + 0.5 * L * la.norm(y - x, 2) ** 2 + reg_coef * la.norm(y, 1)
+
+    w = w0
+    L = L0
+    n = y.size
+
+    X_ = 1 / n * X.T.dot(X)
+    y_ = 1 / n * X.T.dot(y)
+
+    n_iter = 0
+
+    while True :
+
+        L = L / 2
+        w_pred = w
+        grad = (X_.dot(w_pred) - y_)
+        w = prox(w_pred - 1 / L * grad, 1 / L * reg_coef)
+
+        while func(w) > m_L(w, w_pred, grad, L) :
+            L *= 2
+            w = prox(w_pred - 1 / L * grad, 1 / L * reg_coef)
+        L = max(L0, L)
+
+        mu = min(1, reg_coef / la.norm(X_.dot(w) - y_, np.inf)) * (X.dot(w)  - y) / n
+        dual_gap = 0.5 / n * la.norm(X.dot(w) - y) ** 2 + reg_coef * la.norm(w, 1) + 0.5 * n * la.norm(mu) ** 2 + mu.dot(y)
+
+        n_iter += 1
+        if dual_gap < tol or n_iter == max_iter :
+            break
+
+    status = 0 if dual_gap < tol else 1
+    return w, status
 
 
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, -1, 1, 0])
-reg_coef = 1
+reg_coef = 0.01
 w0 = np.array([1, 1])
 w0_plus = np.array([1, 1])
 w0_minus = np.array([2, 2])
 
-w,s = barrier(X, y, reg_coef, w0_plus, w0_minus, max_iter = 100, max_iter_inner = 6)
+w,s = barrier(X, y, reg_coef, w0_plus, w0_minus, max_iter_inner = 6)
+print(w)
+print(s)
+
+w,s = subgrad(X, y, reg_coef, w0)
+print(w)
+print(s)
+
+w,s = prox_grad(X, y, reg_coef, w0)
 print(w)
 print(s)
